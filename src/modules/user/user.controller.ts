bea3a2a54cb80dd9073636e301d1a542c97c7d69
Jeferson Mesquita <je_2742@hotmail.com>
2023-02-13 22:51:10 -0300

    44:26

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,21 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { createUser } from "./user.service";
+import { CreateUserInput } from "./user.schema";
 
-async function registerUserHandler(request: FastifyRequest, reply: FastifyReply){
+async function registerUserHandler(
+  request: FastifyRequest<{ Body: CreateUserInput}>,
+  reply: FastifyReply
+) {
+  const body = request.body;
 
+  try {
+    const user = await createUser(body);
+
+    return reply.code(201).send({ user });
+  } catch (err) {
+    console.log(err);
+    return reply.code(500).send({ err });
+  }
 }
 
-export { registerUserHandler }
+export { registerUserHandler };
